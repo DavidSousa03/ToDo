885365ef0416de0c8d38a86c56d4ec18355bd652
David Sousa <Davidm.sousa244@gmail.com>
2023-12-05 18:26:51 -0300

    salvando uma tarefa no banco de Dados

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ app.set("view engine", "handlebars")
 
 app.use(express.static("public"))
 
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(express.json());
+
+app.post('/criar', (requisicao, resposta) =>{
+    const descricao = requisicao.body.descricao;
+    const completa = 0;
+
+    const sql = `
+        INSERT INTO tarefas(descricao, completa)
+        VALUE ('${descricao}', '${completa}');
+    `
+
+    conexao.query(sql, (erro) =>{
+        if(erro){
+            return console.log(erro);
+        }
+
+        resposta.redirect('/');
+    });
+});
+
 app.get('/', (require, response) => {
     response.render('home')
 })
+
 
 const conexao = mysql.createConnection({
     host: "172.27.131.221",
